2a2dcc2ad9b79b252a60ac384a4c6a91b0e6eec8
MishaPanivnyk <panivnykm@gmail.com>
2024-07-06 14:18:22 +0300

    add bg/footer

--- src/components/Footer/Footer.styled.js
+++ src/components/Footer/Footer.styled.js
@@ -1,8 +1,12 @@
 import styled from 'styled-components';
+import heroBg from '../../img/hero-bg.png';
 
 export const FooterContainer = styled.footer`
   padding-top: 30px;
   padding-bottom: 20px;
+  background: url(${heroBg}), no-repeat center center;
+  background-size: cover;
+  background-color: #0b0e17;
   @media only screen and (min-width: 768px) {
   }
   @media only screen and (min-width: 1024px) {
