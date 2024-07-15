7f8e62aac4e74ef1a7537102385ec2c0af3b5bc7
MishaPanivnyk <panivnykm@gmail.com>
2024-07-15 21:36:03 +0300

    optimaze code and add animation

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,20 +8,39 @@ import {
   HeroTitle,
 } from './Hero.styled';
 import heroImage from '../../img/hero-img.jpg';
+import { motion } from 'framer-motion';
+
+const variants = {
+  hidden: { opacity: 0, y: 50 },
+  visible: { opacity: 1, y: 0 },
+};
 
 export const Hero = () => {
   return (
     <HeroContainer>
       <Container>
-        <HeroPreDesk>Architecture design</HeroPreDesk>
-        <HeroTitle>Professional Interior Design</HeroTitle>
-        <HeroDesk>
-          Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-          veniam quis nostruercitation ullamco laboris
-        </HeroDesk>
-        <HeroBtn>Lets work together</HeroBtn>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={variants}
+          transition={{ duration: 1.5 }}
+        >
+          <HeroPreDesk>Architecture design</HeroPreDesk>
+          <HeroTitle>Professional Interior Design</HeroTitle>
+          <HeroDesk>
+            Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+            veniam quis nostruercitation ullamco laboris
+          </HeroDesk>
+          <HeroBtn>Lets work together</HeroBtn>
+        </motion.div>
       </Container>
-      <HeroImg src={heroImage} alt="Hero" />
+      <motion.div
+        initial={{ opacity: 0, x: 100 }}
+        animate={{ opacity: 1, x: 0 }}
+        transition={{ duration: 1 }}
+      >
+        <HeroImg src={heroImage} alt="Hero" />
+      </motion.div>
     </HeroContainer>
   );
 };
--- src/components/Specializations/Specializations.jsx
+++ src/components/Specializations/Specializations.jsx
@@ -10,6 +10,7 @@ import {
   SpecializationsPreDesk,
   SpecializationsTitle,
 } from './Specializations.styled';
+import { motion } from 'framer-motion';
 
 import SpecializationsImage1 from '../../img/Specializations-1.jpg';
 import SpecializationsImage2 from '../../img/Specializations-2.jpg';
@@ -18,69 +19,82 @@ import SpecializationsImage4 from '../../img/Specializations-4.jpg';
 import 'swiper/css';
 import 'swiper/css/effect-coverflow';
 
+const containerVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.8,
+      staggerChildren: 0.3,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, x: -100 },
+  visible: { opacity: 1, x: 0, transition: { duration: 2 } },
+};
+
 export const Specializations = () => {
   return (
-    <>
-      <SpecializationsContainer>
-        <Container>
+    <SpecializationsContainer>
+      <Container>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+        >
           <SpecializationsPreDesk>TOP SERVICES</SpecializationsPreDesk>
           <SpecializationsTitle>Our Specializations</SpecializationsTitle>
-        </Container>
-        <Swiper
-          effect={'coverflow'}
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={'auto'}
-          coverflowEffect={{
-            rotate: 50,
-            stretch: 0,
-            depth: 100,
-            modifier: 1,
-            slideShadows: true,
-          }}
-          modules={[EffectCoverflow]}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <SpecializationsImg src={SpecializationsImage1} alt="Nature 1" />
-            <SpecializationsImgTextContainer>
-              <SpecializationsImgTitle>Floor plan</SpecializationsImgTitle>
-              <SpecializationsImgDesc>
-                Tempor incididunt ut labore et dolore magna aliqua.
-              </SpecializationsImgDesc>
-            </SpecializationsImgTextContainer>
+        </motion.div>
+      </Container>
+      <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        modules={[EffectCoverflow]}
+        className="mySwiper"
+      >
+        {[
+          SpecializationsImage1,
+          SpecializationsImage2,
+          SpecializationsImage3,
+          SpecializationsImage4,
+        ].map((image, index) => (
+          <SwiperSlide key={index}>
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={itemVariants}
+            >
+              <SpecializationsImg
+                src={image}
+                alt={`Specialization ${index + 1}`}
+              />
+              <SpecializationsImgTextContainer>
+                <SpecializationsImgTitle>
+                  {index === 0 && 'Floor plan'}
+                  {index === 1 && 'Interior Design'}
+                  {index === 2 && 'Architecture Design'}
+                  {index === 3 && 'Construction'}
+                </SpecializationsImgTitle>
+                <SpecializationsImgDesc>
+                  Tempor incididunt ut labore et dolore magna aliqua.
+                </SpecializationsImgDesc>
+              </SpecializationsImgTextContainer>
+            </motion.div>
           </SwiperSlide>
-          <SwiperSlide>
-            <SpecializationsImg src={SpecializationsImage2} alt="Nature 2" />
-            <SpecializationsImgTextContainer>
-              <SpecializationsImgTitle>Interior Design</SpecializationsImgTitle>
-              <SpecializationsImgDesc>
-                Tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-              </SpecializationsImgDesc>
-            </SpecializationsImgTextContainer>
-          </SwiperSlide>
-          <SwiperSlide>
-            <SpecializationsImg src={SpecializationsImage3} alt="Nature 3" />
-            <SpecializationsImgTextContainer>
-              <SpecializationsImgTitle>
-                Architecture Design
-              </SpecializationsImgTitle>
-              <SpecializationsImgDesc>
-                Tempor incididunt ut labore et dolore magna aliqua.
-              </SpecializationsImgDesc>
-            </SpecializationsImgTextContainer>
-          </SwiperSlide>
-          <SwiperSlide>
-            <SpecializationsImg src={SpecializationsImage4} alt="Nature 4" />
-            <SpecializationsImgTextContainer>
-              <SpecializationsImgTitle>Construction</SpecializationsImgTitle>
-              <SpecializationsImgDesc>
-                Tempor incididunt ut labore et dolore magna aliqua.
-              </SpecializationsImgDesc>
-            </SpecializationsImgTextContainer>
-          </SwiperSlide>
-        </Swiper>
-      </SpecializationsContainer>
-    </>
+        ))}
+      </Swiper>
+    </SpecializationsContainer>
   );
 };
