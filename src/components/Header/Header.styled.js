4e59afb052c23bae6dd37241129df9a218a309bb
MishaPanivnyk <panivnykm@gmail.com>
2024-07-15 21:35:41 +0300

    add animation

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -1,5 +1,25 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
+const fadeInLogo = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(-20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+const fadeInBurger = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(20px);
+  }
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
 export const HeaderContainer = styled.header`
   padding: 50px 0px;
   @media only screen and (min-width: 768px) {
@@ -21,6 +41,7 @@ export const HeaderLogoLink = styled.a`
     width: 66px;
     height: 24px;
     transition: all 500ms cubic-bezier(0.4, 0, 0.2, 1);
+    animation: ${fadeInLogo} 1s ease-out;
   }
   &:hover svg {
     transform: scale(1.2);
@@ -35,5 +56,6 @@ export const HeaderBurger = styled.button`
     width: 27px;
     height: 24px;
     transition: all 500ms cubic-bezier(0.4, 0, 0.2, 1);
+    animation: ${fadeInBurger} 1s ease-out;
   }
 `;
